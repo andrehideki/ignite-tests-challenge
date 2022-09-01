d6e2573176d5f10162cbb3cc3c956a1881fb586f
andrehideki <andre.hkiw.dev@gmail.com>
2022-09-01 16:44:53 -0300

    test: [POST] /api/v1/statements/withdraw - Should create a with retrieving 201

--- src/modules/statements/useCases/createStatement/CreateStatementController.spec.ts
+++ src/modules/statements/useCases/createStatement/CreateStatementController.spec.ts
@@ -1,4 +1,4 @@
-import { createConnection } from "typeorm";
+import { Connection, createConnection } from "typeorm";
 
 import request from "supertest";
 import { User } from "../../../users/entities/User";
@@ -7,8 +7,13 @@ import { Statement } from "../../entities/Statement";
 
 describe("CreateStatementController", () => {
 
+    var conn: Connection;
+
+    beforeAll(async () => {
+        conn = await createConnection();
+    })
+
     beforeEach(async () => {
-        const conn = await createConnection();
         await conn.getRepository(Statement).delete({});
         await conn.getRepository(User).delete({});
         await request(app).post("/api/v1/users")
@@ -27,6 +32,24 @@ describe("CreateStatementController", () => {
             .send({
                 amount: 10,
                 description: "deposit"
+            })
+            .set({ authorization: `Bearer ${token}` })
+            .expect(201);
+    });
+    
+    test("[POST] /api/v1/statements/withdraw - Should create a with retrieving 201", async () => {
+        const { token } = (await request(app).post("/api/v1/sessions")
+            .send({ email: "test@mail.com", password: "1234" })).body;
+        await request(app).post("/api/v1/statements/deposit")
+        .send({
+            amount: 10,
+            description: "deposit"
+        })
+        .set({ authorization: `Bearer ${token}` });
+        await request(app).post("/api/v1/statements/withdraw")
+            .send({
+                amount: 5,
+                description: "withdraw"
             })
             .set({ authorization: `Bearer ${token}` })
             .expect(201);
